89a4669214518733d7adf9d8f7e41fcf82f5d041
Nikki <nicleemart@gmail.com>
2016-08-30 21:32:16 -0500

    put in window load event

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,19 @@
-for (var i = 1; i <= 100; i++) {
-	var isDividibleByThree = i % 3 === 0;
-	var isDivisibleByFive = i % 5 === 0;
+window.addEventListener("load", function () {
+	for (var i = 1; i <= 100; i++) {
+		var isDivisibleByThree = i % 3 === 0;
+		var isDivisibleByFive = i % 5 === 0;
 
-	if (isDividibleByThree && isDivisibleByFive) {
-		console.log('FizzBuzz');
+		if (isDivisibleByThree && isDivisibleByFive) {
+			console.log("FizzBuzz");
+		}
+		else if (isDivisibleByThree) {
+			console.log("Fizz");
+		}
+		else if (isDivisibleByFive) {
+			console.log("Buzz");
+		}
+		else {
+			console.log(i);
+		}
 	}
-	else if (isDividibleByThree) {
-		console.log('Fizz');
-	}
-	else if (isDivisibleByFive) {
-		console.log('Buzz');
-	}
-	else {
-		console.log(i);
-	}
-}
+});
